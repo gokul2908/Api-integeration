874bf381e0d8a83194dfbe62ef5433d40582fa81
gokul2908 <gokulkumar2908@gmail.com>
2022-09-08 21:52:02 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from "axios";
+import { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    fetchApiData();
+  }, []);
+  async function fetchApiData() {
+    const { data: meals } = await axios.get(
+      "https://www.themealdb.com/api/json/v1/1/search.php?f=a"
+    );
+    setData(meals.meals);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      {data.map((e) => {
+        const { idMeal, strMealThumb, strMeal, strYoutube, strInstructions } =
+          e;
+        return (
+          <div className="card" style={{ width: "18rem" }} key={idMeal}>
+            <img
+              className="card-img-top"
+              src={strMealThumb}
+              alt="Card image cap"
+            />
+            <div className="card-body">
+              <h5 className="card-title">{strMeal}</h5>
+              <p className="card-text">{strInstructions.slice(0, 100).concat("...")}</p>
+              <div className="button_ref">
+                <a href={strYoutube} className="btn btn-primary">
+                  Youtube
+                </a>
+                <a href={strYoutube} className="btn btn-primary">
+                  Know more
+                </a>
+              </div>
+            </div>
+          </div>
+        );
+      })}
     </div>
   );
 }
